8bc264102ae0d5c254ef4039e404f56602e3ec04
anwarkhan0 <anwarsyed01234@gmail.com>
2025-11-16 09:25:18 +0300

    324325

--- src/App.jsx
+++ src/App.jsx
@@ -396,7 +396,7 @@ const Portfolio = () => {
                   }`}
                 >
                   <Linkedin className="w-6 h-6 text-purple-500" />
-                  <span>linkedin.com/in/SyedAnwar</span>
+                  <span>linkedin.com/in/SyedAnwar000</span>
                 </a>
               </div>
             </div>
